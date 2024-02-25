a8d11667b9072d17d14e7b230a6d860e935a17f6
dlwlsgml <jinhee020928@naver.com>
2024-02-25 23:24:47 +0900

    Update GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+   
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public void OnClickStartButton ()
     {
-        
+        CoverImage.setActive(false);
+        p
     }
 }
