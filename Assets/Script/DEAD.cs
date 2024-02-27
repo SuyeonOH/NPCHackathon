71202f0aeb3fc73947bdf118a765daebcb5bbd22
20210864 <114654926+20210864@users.noreply.github.com>
2024-02-27 23:46:39 +0900

    Add files via upload
    속도 조절

--- Assets/Script/DEAD.cs
+++ Assets/Script/DEAD.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DEAD : MonoBehaviour
 {
-    [SerializeField] private float speed = 7f;
+    [SerializeField] private float speed = 70f;
 
     // Update is called once per frame
     void Update()
     {
+        if (400 <= PLAYER.instance.score)
+            speed = 110f;
+        else if (300 <= PLAYER.instance.score)
+            speed = 100f;
+        else if (200 <= PLAYER.instance.score)
+            speed = 90f;
+        else if (100 <= PLAYER.instance.score)
+            speed = 80f;
         transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
 
         if (transform.position.x <= -177)
--- Assets/Script/ITEM.cs
+++ Assets/Script/ITEM.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class ITEM : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 50f;
+    [SerializeField] private float speed = 70f;
     private int score = 10;
 
     // Update is called once per frame
     void Update()
     {
-        if (200 <= PLAYER.instance.score)
+        if (400 <= PLAYER.instance.score)
+            speed = 110f;
+        else if (300 <= PLAYER.instance.score)
+            speed = 100f;
+        else if (200 <= PLAYER.instance.score)
             speed = 90f;
-        else if (150 <= PLAYER.instance.score)
-            speed = 80f;
         else if (100 <= PLAYER.instance.score)
-            speed = 70f;
-        else if (50 <= PLAYER.instance.score)
-            speed = 60f;
-
+            speed = 80f;
         transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
 
         if (transform.position.x <= -177)
