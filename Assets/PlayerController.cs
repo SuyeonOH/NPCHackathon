a96bd2575fe62efedbe0eb1599d8288c8974f1ab
SuyeonOH <103015570+SuyeonOH@users.noreply.github.com>
2024-02-27 20:31:01 +0900

    교수님 점프 속도 높이 조절

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour
     private bool isJumping = false;
     private bool isFalling = false;
     private bool canJump = true;
-    private float jumpHeight = 20.0f;
-    private float jumpSpeed = 50.0f;
+    private float jumpHeight = 40.0f;
+    private float jumpSpeed = 80.0f;
     private Vector2 startPosition;
 
     void Start()
